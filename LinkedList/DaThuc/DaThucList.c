20b5b3e99773521d3df1416621823aec922c79fd
TranHuy2k2 <giahuytran2002@gmail.com>
2021-09-13 02:56:34 +0700

    Completed LinkedList

--- LinkedList/DaThuc/DaThucList.c
+++ LinkedList/DaThuc/DaThucList.c
@@ -21,6 +21,9 @@ DaThuc khoitao();
 void chenDonThuc(DonThuc s, DaThuc *pL);
 DaThuc congDaThuc(DaThuc D1, DaThuc D2);
 void inDaThuc(DaThuc d);
+DaThuc nhapDaThuc();
+DaThuc nhanDaThuc(DaThuc D1, DaThuc D2);
+DaThuc tinhDaoHam(DaThuc d);
 
 // Functions
 DaThuc khoitao()
@@ -28,6 +31,19 @@ DaThuc khoitao()
     Position N = (Position)malloc(sizeof(struct Node));
     N->Next = NULL;
     return N;
+}
+DaThuc nhapDaThuc()
+{
+    int n;
+    scanf("%d", &n);
+    DaThuc L = khoitao();
+    for (int i = 0; i < n; i++)
+    {
+        DonThuc s;
+        scanf("%lf%d", &s.he_so, &s.bac);
+        chenDonThuc(s, &L);
+    }
+    return L;
 }
 void chenDonThuc(DonThuc s, DaThuc *pL)
 {
@@ -74,6 +90,26 @@ DaThuc congDaThuc(DaThuc D1, DaThuc D2)
     }
     return D3;
 }
+DaThuc nhanDaThuc(DaThuc D1, DaThuc D2)
+{
+    Position p = D1, q = D2, D3 = khoitao();
+    while (p->Next != NULL)
+    {
+        DonThuc donThucP = p->Next->e;
+        q = D2;
+        while (q->Next != NULL)
+        {
+            DonThuc donThucQ = q->Next->e;
+            DonThuc tmp;
+            tmp.he_so = donThucP.he_so * donThucQ.he_so;
+            tmp.bac = donThucP.bac + donThucQ.bac;
+            chenDonThuc(tmp, &D3);
+            q = q->Next;
+        }
+        p = p->Next;
+    }
+    return D3;
+}
 void inDaThuc(DaThuc d)
 {
     Position p = d;
@@ -87,4 +123,18 @@ void inDaThuc(DaThuc d)
         }
         p = p->Next;
     }
+}
+DaThuc tinhDaoHam(DaThuc d)
+{
+    DaThuc kq = khoitao();
+    Position p = d;
+    while (p->Next != NULL)
+    {
+        DonThuc current = p->Next->e;
+        current.he_so *= current.bac;
+        current.bac--;
+        chenDonThuc(current, &kq);
+        p = p->Next;
+    }
+    return kq;
 }
