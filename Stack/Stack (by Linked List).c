af43f076d2af6e019426c0609ffdbd9f6c9bdd5b
TranHuy2k2 <giahuytran2002@gmail.com>
2021-09-14 13:23:00 +0700

    Move define.h into DoublyLinkedList, move PListLib.c to Stack

--- Stack/Stack (by Linked List).c
+++ Stack/Stack (by Linked List).c
@@ -1,43 +1,50 @@
-# include <stdio.h>
-# include <math.h>
-# include <stdlib.h>
-# include <string.h>
-# include "PListLib.c"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include "PListLib.c"
 
 // khoi tao Stack bang LinkedList
 typedef List Stack;
 
 // khoi tao Stack rong
-void makenullStack(Stack *pS){
+void makenullStack(Stack *pS)
+{
 	makenullList(pS);
 }
 
 // kiem tra Stack rong
-int emptyStack(Stack S){
+int emptyStack(Stack S)
+{
 	return emptyList(S);
 }
 
 // tra ve p.tu o dinh Stack
-ElementType top(Stack S){
-	retrieve(first(S),S);
+ElementType top(Stack S)
+{
+	retrieve(first(S), S);
 }
 
 // xoa p.tu tai dinh Stack
-void pop(Stack *pS){
+void pop(Stack *pS)
+{
 	deleteList(first(*pS), pS);
 }
 
 // them p.tu vao dinh Stack
-void push(Elementtype x, Stack *pS){
-	insertList(x,first(*pS), pS);
+void push(ElementType x, Stack *pS)
+{
+	insertList(x, first(*pS), pS);
 }
 
 // in cac p.tu trong Stack
-void printStack(Stack S){
+void printStack(Stack S)
+{
 	printList(S);
 }
 
 // nhap Stack tu ban phim
-void readStack(Stack *pS){
+void readStack(Stack *pS)
+{
 	// tao chua lam ok ?
 }
