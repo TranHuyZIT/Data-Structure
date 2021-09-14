29ca3efaf0a2c4860296c661d4a464a16de1bed4
Khoi <89968407+DuyKhoiHuynh@users.noreply.github.com>
2021-09-14 19:23:32 +0700

    Update Stack

--- Stack/Stack (by Array).c
+++ Stack/Stack (by Array).c
@@ -3,64 +3,67 @@
 # include <stdlib.h>
 # include <string.h>
 
-// do dai toi da cua Stack
+// khoi tao Stack
 #define MaxLength 100
 
-// kieu p.tu
 typedef int ElementType;
 
-// khoi tao Stack
 typedef struct{
-	/*dung Array de luu tru
-	cac p.tu*/
 	ElementType Elements[MaxLength];
 
-	// dinh cua Stack
 	int Top_idx;
 }Stack;
 
-// khoi tao Stack rong
+// prototypes:
+
+void makenullStack(Stack *pS) 		// khoi tao Stack rong
+int emptyStack(Stack S) 			// kiem tra Stack rong
+int full(Stack S) 					// kiem tra Stack day
+ElementType top(Stack S) 			// tra ve p.tu tai dinh Stack
+void pop(Stack *pS) 				// xoa p.tu tai dinh Stack
+void push(ElementType x, Stack *pS) // them p.tu vao dinh Stack
+void printStack(Stack S) 			// in tat ca p.tu co trong Stack
+void readStack(Stack *pS) 			// nhap Stack tu ban phim
+
+
+// functions:
+
 void makenullStack(Stack *pS){
 	pS->Top_idx = MaxLength;
 }
 
-// kiem tra Stack rong
 int emptyStack(Stack S){
 	return S.Top_idx == MaxLength;
 }
 
-// kiem tra Stack day
 int full(Stack S){
 	return S.Top_idx == 0;
 }
 
-// tra ve p.tu tai dinh Stack
 ElementType top(Stack S){
 	// neu Stack rong -> loi
 	if (emptyStack(S)){
-		printf("Stack is empty bitch!\n");
+		printf("Stack is empty\n");
 	}
 	else{
 		return S.Elements[S.Top_idx];
 	}
 }
 
-// xoa p.tu tai dinh Stack
 void pop(Stack *pS){
 	// neu Stack rong -> loi
 	if (emptyStack(*pS)){
-		printf("Stack is empty bitch!\n");
+		printf("Stack is empty\n");
 	}
 	else{
 		pS->Top_idx = pS->Top_idx +1;
 	}
 }
 
-// them p.tu vao dinh Stack
 void push(ElementType x, Stack *pS){
 	// neu Stack day -> loi
 	if (full(*pS)){
-		printf("Stack is full bitch!\n");
+		printf("Stack is full\n");
 	}
 	else{
 		pS->Top_idx = pS->Top_idx-1;
@@ -68,11 +71,10 @@ void push(ElementType x, Stack *pS){
 	}
 }
 
-// in tat ca p.tu co trong Stack
 void printStack(Stack S){
 	// neu Stack day -> loi
 	if (emptyStack(S)){
-		printf("Stack is empty bitch!\n");
+		printf("Stack is empty\n");
 	}
 	else{
 		for (int i=S.Top_idx; i<MaxLength; i++){
@@ -82,7 +84,6 @@ void printStack(Stack S){
 	printf("\n");
 }
 
-// nhap Stack tu ban phim
 void readStack(Stack *pS){
 	makenullStack(pS);
 
@@ -93,4 +94,12 @@ void readStack(Stack *pS){
 		scanf("%d", &pS->Elements[i]);
 		pS->Top_idx--;
 	}
+}
+
+int main(){
+	Stack stack;
+	makenullStack(&stack);
+
+
+	return 0;
 }
